f384dd66bee69ee3f697d85599f6dc3319ec6bb1
Ty <none>
2022-07-29 17:35:14 -0700

    porting the windows version to here. can put things in subfolders (ex: wp_sunsets_big-red-one1231.jpg looks for sunsets subfolder inside of wp). Is a little buggy, needs TLC and simplification I think

--- suos.py
+++ suos.py
@@ -1,114 +1,210 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 #                 SUOS
 # the latin word 'suos' means 'their own'
 #
 # use .suos to change settings
-# place .suos in the same directory as this script.
+# place .suos in the same directory as this script.import os
 
-#boolean flags
 debugging_mode = False
+standard_mode  = True
+verbose_mode   = True
+manual_mode    = False
 reset_mode     = False
-verbose_mode   = False
 
-logfile = "/home/ty/automation/reports/suos.log"
-movedfiles = 0
 
-import os, sys, time, glob, logging
 
-logging.basicConfig(filename=f'{logfile}', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level=logging.NOTSET)
+import os, sys, time, glob, random
+import shutil, string
+
+def print_help_text():
+	print("SUOS HELP TEXT BRO ITS RIGHT HERE")
+	print("flags: --dry, --verbose, --reset, --manual")
+	print("todo: put more information here")
+	exit(0)
+
+def search_replace(base_string, search_for, replacement_with):
+    result=''
+    if base_string == '' or search_for == '': return result
+    for character in base_string:
+        if character in search_for: result+=replacement_with
+        else: result+=character
+    return result
+
+def manual(setting):
+    if verbose_mode == True: print("----entering manual() function")
+    def namegen():
+        n=''
+        for i in range(1,12):
+            n+=str(random.randint(0, 9))
+        #print(f'n: {n}')
+        return n
+    #this would be a good place to go through a special folder like
+    #/pornstart/unsorted/sort-LABEL where EACH LABEL TYPE got a folder
+    #for manual sorting. Anything in there gets moved to the proper
+    #storage spot with a generic name ala uhm_0001 or joi_0121
+    #copy paste sorting then run "suos.py --unsorted"
+    #could even just rename them and move them to /downloads/ or something
+    #to be later integrated?
+    #expand movearr with the default-named-manually-sorted-fellas
+    label, destination, count, ignore = setting
+    try:
+        manual_sort_spot=f"{destination}{sep}manual{sep}"
+        print(f"manual_sort_spot: {manual_sort_spot}")
+        #os.chdir(manual_sort_spot)
+        movarr=glob.glob(f'{manual_sort_spot}*')
+        count=0
+
+        fileprocess(label, destination, count, movarr)
+        newmov = []
+        for file in movarr:
+            if os.path.isdir(file): continue
+            count+=1
+            extension=file.split(sep)[-1] #Just the basename
+            if len(extension.split(".")) > 1:
+                extension=extension.split(".")[-1]
+                extension=f".{prefix}"
+            else:
+                extension=''
+            newname=f"{manual_sort_spot}" + namegen() + extension
+            #print(f"name: {name}")
+            if debugging_mode:
+                print(f'Would do: [{file}] -> [{newname}]')
+            else:
+                if verbose_mode == True: print(f'DOING: [{file}] -> [{newname}]')
+                os.rename(f"{file}", f"{newname}")
+            newmov.append(newname)
+        if verbose_mode == True: print("manual mode finished w/the movarr loop")
+        fileprocess(label, destination, count, newmov)
+        return 0
+    except Exception as ex:
+        print("caught errors:")
+        print(ex)
+    exit(11)
 
 ## Go through each label's settings
 ## and move files
 ## from source/old_name -> destination/new_name
 def standard(setting):
-    if verbose_mode == True: print("entering standard() function")
+    if verbose_mode == True: print("----entering standard() function")
     label, destination, count, movarr = setting
-    fileprocess(label, destination, count, movarr)    
+    fileprocess(label, destination, count, movarr)
 
 def reset(setting):
-	
-    if verbose_mode == True: print("entering reset() function")
+    print("implementation dangerous and Idk man what was this for?")
+    return 1
+    exit(0)
+"""
+    if verbose_mode == True: print("----entering reset() function")
     label, destination, count, ignored_setting = setting
     movarr=glob.glob(f'{destination}{sep}*')
     llength=len(label)
+
     for name in movarr:
         basename=name.split(sep)[-1]
-        if not label in basename[0:llength-1]:
+        if not label in basename[0:llength]:
+            #print(f'basename: {basename}')
             name=f'{destination}{sep}{label}{basename}'
+            #print(f'name: {name}')
     count=-1
     fileprocess(label, destination, count, movarr)
-
+"""
 def fileprocess(label, destination, count, movarr):
-    global movedfiles
     if verbose_mode == True:
-        print("settings look like this:")
-        print("label: " + label)
-        print("destination: " + destination)
-        print("count: " + str(count))
-        print("movarr: " + str(movarr))
+        print("----filepocess() received:")
+        print("--label: " + label)
+        print("--destination: " + destination)
+        print("--count: " + str(count))
+        print("--movarr: " + str(movarr))
     for file in movarr:
+        if not os.path.isfile(file): continue
+        basename=file.split(sep)[-1]
         try:
-            suffix= "." + file.split(".")[1]
-        except: suffix = ""
+            suffix= "." + basename.split(".")[-1]
+        except:
+            suffix = ""
         count+=1
-        cstring = str(count).rjust(4,"0")
+        count_string = str(count).rjust(4,"0")
         tag=''
         optional_under='_'
-        basename=file.split(sep)[-1]
-        for c in basename.lower().strip(label).strip("_"):
+        optional_sub_folders=''
+        #detect if tag is present in the basename,
+        #               only allow certain characters into tag (A-z_-)
+        #               and if it was terminated with '_' or needs it still
+        for c in basename.lower().lstrip(label).lstrip("_"):
             if c == '.': break
-            if c.isalpha() or c in '_-':  tag+=c
+            elif c.isalpha() or c in '-_':  tag+=c
+            elif c == ' ': tag+='-'
         try:
             if tag == '' or tag[-1] == "_": optional_under=''
         except: optional_under = ''
-        newfilepath = f'{destination}{sep}{label}_{tag}{optional_under}{cstring}{suffix}'
-        if debugging_mode: print(f'Would do: {file} ---> {newfilepath}')
+
+        #CHECK FOR ALTERNATE DESTINATIONS NOW
+        #w/in the destination (/joi/) look for a SUBFOLDER sharing name w/tag
+        tags=tag.split('_')
+        for t in tags:
+            subfolder=f'{sep}{t}'
+            if os.path.isdir(f'{destination}{optional_sub_folders}{subfolder}'):
+                optional_sub_folders+=subfolder
+            else: break
+
+        newfilepath = \
+        f'{destination}{optional_sub_folders}{sep}{label}_{tag}{optional_under}{count_string}{suffix}'
+
+        if debugging_mode:
+            print(f'Would do: {file} ---> {newfilepath}')
         else:
-            os.rename(file, newfilepath)
-            movedfiles = movedfiles + 1
-        
-    
+            if verbose_mode: print(f'trying: shutil.move({file},{newfilepath}')
+            shutil.move(file,newfilepath)
+            #os.renames(file, newfilepath)
+
+
 ############################## END OF FUNCTIONS ##############################
 ############################## START OF SCRIPT  ##############################
 
+if verbose_mode: print("end functions/start script")
 
 # necessary when calling from mu, which has
 # its own "home directory" or whatever
 #os.chdir('/home/ty/bin/mu_code/')
 args = sys.argv
-for arg in args[1:]:
-    if verbose_mode == True: print(f"argument: {arg}")
-    if "dry" in arg or "dryrun" in arg or "--dry" in arg or "-d" in arg: 
+if len(args) > 1:
+    if "help" in args or "--help" in args or "-h" in args:
+        print_help_text()
+    if "dry" in args or "dryrun" in args or "--dry" in args or "-d" in args:
         debugging_mode = True
-    elif "reset" in arg or "resetrun" in arg or "--reset" in arg or "-r" in arg:
+    if "debug" in args or "--debug" in args:
+        debugging_mode = True
+    if "reset" in args or "resetrun" in args or "--reset" in args or "-r" in args:
         reset_mode = True
-    elif "verbose" in arg or "verboserun" in arg or "--verbose" in arg or "-v" in arg:
+    if "verbose" in args or "verboserun" in args or "--verbose" in args or "-v" in args:
         verbose_mode = True
-    else:
-        print("Command line usage: --dry and --reset modes.")
-        print("Call again with no arguments and it'll just do the standard stuff")
-        exit(0)
+    if "manual" in args or "manualrun" in args or "--manual" in args or "-m" in args:
+        manual_mode = True
+    #print("Command line usage: --dry and --reset modes.")
+    #print("Call again with no arguments and it'll just do the standard stuff")
+    #exit(0)
 
-sep= os.path.sep
-newline= f'\n'
-user_name= os.environ.get('USER') #didn't fix, just hardcoded 'Ty'
-
+sep = os.path.sep
+newline = f'\n'
+user_name = os.environ.get('USER') #didn't fix, just hardcoded 'Ty' ??? but now?
 
 #must point to where script is saved
-scr_dir = os.path.join(sep, 'home', user_name, 'bin')
+#"C:\Users\Ty\Documents\scripts"
 #for windows, change to TOP LEVEL of whatever directory structure.
+scr_file = os.path.realpath(__file__)
+scr_dir =  os.path.dirname(scr_file)
 #for instance might be /programfiles/uninstallinformation/
-home_dir= os.path.join(sep, 'home', user_name) #NOT USED??
+#doesn't seem to be needed? # home_dir= os.path.join(sep, 'home', user_name)
 
 
-settings_file= open(f'{scr_dir}{sep}.suos', 'r')
+settings_file = open(f'{scr_dir}{sep}.suos', 'r')
 lines = settings_file.readlines()
 settings = []
 sources = []
+if verbose_mode: print("scanning .suos file")
 
 ## Scans .suos file to build up script settings
 for l in lines:
-    newline=f'\n'
     l = l.rstrip(newline)
     words= l.split(";")
     #skippable lines (comments and blank lines)
@@ -116,16 +212,24 @@ for l in lines:
 
     if (l[0:5] == "source" or words[0] == "source"): # SOURCE
         sources.append(words[1])
-        if verbose_mode == True: print(f"source: {words[1]}")
+        #print(words[1])
         continue
     else: #label - destination pairs
         label = words[0]
         destination = words[1].rstrip(newline)
-        count = len(glob.glob(f'{destination}{sep}{label}*'))
+        if os.path.isdir(destination) == True: print(f'{destination} exists bro')
+        else:
+            print("it don't exist")
+            os.mkdir(destination)
+            if os.path.isdir(destination) == True:
+                print(f'{destination} exists NOW.')
+            else: print(f"creation of {destination} FAILED")
+        count = len(glob.glob(f'{destination}{sep}**', recursive=True))
         entry = [label, destination, count, []]
         settings.append(entry)
 
 settings_file.close()
+if verbose_mode: print("closed .suos file")
 
 ##  BUILD THE MATCHED FILES LIST
 ##  FOR EACH OF THE LABELS and
@@ -135,27 +239,29 @@ for source in sources:
         os.chdir(os.path.normpath(source))
     except: continue
     cwd= str(os.path.abspath(os.getcwd()))
-    for check in settings:
+    for set in settings:
         matched_files=[]
-        label, count = (check[0], check[1])
-        #destination = check[2] # an array of destinations... ??? wat? Idk man
+        label, count = (set[0], set[1])
         for found in glob.glob(f'{label}*'):
             fullpath=f'{cwd}{sep}{found}'
             matched_files.append(fullpath)
-        check[3].extend(matched_files)
+        set[3].extend(matched_files)
+
+if verbose_mode: print("--done setting up--")
 
 if debugging_mode == True:
     print("settings: ")
     for s in settings:
         print(str(s))
     print()
-elif reset_mode == True:
+
+if reset_mode == True:
     for s in settings:
         reset(s)
-else:
+if manual_mode == True:
+    for s in settings:
+        manual(s)
+if standard_mode == True:
     for s in settings:
         standard(s)
-        
-if (movedfiles > 0):
-	logging.info(f'{time.strftime("%c")}: Moved {movedfiles} files')
-#else: logging.info('TEST') #just for testing when no files to move
+
